eda9832e59ae3cd705150f701317e5610358b84b
hyobbeee <yjh5799@naver.com>
2022-07-11 22:31:34 +0900

    git first setting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import data from './route/data.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [player] = useState(data);
+	return (
+		<>
+			<Navbar bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand href="#home">AT Market</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#features">Features</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+			<div className="main-bg"></div>
+			<div className="container">
+				<div className="row">
+					{/* {player.map(function (a, i) {
+						return <List player={player[i]} i={i} />;
+					})} */}
+					<List />;
+				</div>
+			</div>
+		</>
+	);
 }
 
+function List(props) {
+	return (
+		<div className="col-md-4">
+			<Nav.Link href={'detail/' + props.i} style={{ display: 'inline-block' }}>
+				<img
+					src={process.env.PUBLIC_URL + '/' + props.i + '.png'}
+					width="70%"
+					height="200px"
+					alt=""
+				/>
+			</Nav.Link>
+			<h4>{props.player.name}</h4>
+			<p>{props.player.age}</p>
+			<p>{props.player.position}</p>
+		</div>
+	);
+}
 export default App;
---
+++ src/route/data.js
@@ -0,0 +1,20 @@
+let data = [
+	{
+		id: 0,
+		name: 'João Félix',
+		age: '1999년 11월 10일',
+		position: 'Forward',
+	},
+	{
+		id: 1,
+		name: 'Marcos Llorente',
+		age: '1995년 1월 30일',
+		position: 'Midfielder',
+	},
+	{
+		id: 2,
+		name: 'Luis Diaz',
+		age: '1987년 1월 24일',
+		position: 'Forward',
+	},
+];
